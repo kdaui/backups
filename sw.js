409afd7e9432c77f995131d15c0431b653150916
kdaui <bilal.q2020@gmail.com>
2026-03-28 19:11:17 +0000

    Add PWA support with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'backups-v1';
+const urlsToCache = [
+  '/backups/',
+  '/backups/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
